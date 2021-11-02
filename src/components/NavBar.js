ebae2b12f334952f4c6c95b66793d0d00c65c431
Austin Chau <austin.chau1@gmail.com>
2021-11-02 00:21:14 -0400

    Basic project files added

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <h3>Logo</h3>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Cart 0</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
